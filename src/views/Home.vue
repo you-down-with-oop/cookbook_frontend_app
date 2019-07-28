a7f0568c1db0caf4dc7f4090e21b476cbbeb6be4
Peter Jang <peter.x.jang@gmail.com>
2019-07-28 10:57:26 -0500

    Add image and recipes new/create (unfinished)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="home">
+    <h1>New Recipe</h1>
+    <button v-on:click="createRecipe()">Create</button>
     <h1>{{ message }}</h1>
     <div v-for="recipe in recipes">
       <h2>{{ recipe.title }}</h2>
+      <img v-bind:src="recipe.image_url" alt="" />
       <p>Ingredients: {{ recipe.ingredients }}</p>
       <p>Directions: {{ recipe.directions }}</p>
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+img {
+  width: 100%;
+}
+</style>
 
 <script>
 import axios from "axios";
@@ -27,6 +34,22 @@ export default {
       console.log(this.recipes);
     });
   },
-  methods: {}
+  methods: {
+    createRecipe: function() {
+      console.log("Create the recipe...");
+      var params = {
+        title: "THE Example title",
+        chef: "Example chef",
+        prep_time: 100,
+        ingredients: "Example ingredients",
+        directions: "Example directions",
+        image_url: "example image_url"
+      };
+      axios.post("/api/recipes", params).then(response => {
+        console.log("Success", response.data);
+        this.recipes.push(response.data);
+      });
+    }
+  }
 };
 </script>
