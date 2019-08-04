4dc2c851951726dc55d0230cffede07ea5d52d2e
Peter Jang <peter.x.jang@gmail.com>
2019-08-04 09:51:02 -0500

    Add buttons to sort by title and prep time

--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="container home">
+    <div>
+      <button v-on:click="sortAttribute = 'title'" class="btn btn-primary">Sort by title</button>
+      <button v-on:click="sortAttribute = 'prep_time'" class="btn btn-primary">Sort by prep time</button>
+    </div>
+    <br />
     Search:
     <input v-model="searchFilter" type="text" list="titles" />
     <datalist id="titles">
       <option v-for="recipe in recipes">{{ recipe.title }}</option>
     </datalist>
     <div class="row">
-      <div v-for="recipe in filterBy(recipes, searchFilter, 'title', 'chef')" class="col-sm-4">
+      <div v-for="recipe in orderBy(filterBy(recipes, searchFilter, 'title'), sortAttribute)" class="col-sm-4">
         <div class="card">
           <img v-bind:src="recipe.image_url" class="card-img-top" alt="..." />
           <div class="card-body">
             <h2 class="card-title">{{ recipe.title }}</h2>
             <p>by {{ recipe.chef }}</p>
+            <p>Prep time: {{ recipe.prep_time }}</p>
             <router-link v-bind:to="`/recipes/${recipe.id}`">More info</router-link>
           </div>
         </div>
@@ -39,7 +45,8 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       recipes: [],
-      searchFilter: ""
+      searchFilter: "",
+      sortAttribute: "title"
     };
   },
   created: function() {
